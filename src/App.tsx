f07ab2be0e587b0b81def9e53df67e27fce2a9f6
Wing Bui <38932460+wingbui@users.noreply.github.com>
2022-08-25 14:05:15 -0700

    build: styling the app container and scrollbars

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import styled from "@emotion/styled";
 import { QRReader } from "./pages/qrReader/QRReader";
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <QRReader />
-    </div>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  max-width: 1224px;
+  margin: auto;
+  padding: 1rem;
+`;
 
 export default App;
